9ff0cd55ae313abcba50e1f2fcdb84a79f185ed3
Alex Verbitsky <alexandruverbitsky@alexs-air.home>
2023-08-10 12:02:46 -0400

    Derived state of userInput

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './assets/investment-calculator-logo.png';
-import Header from './components/Header/Header';
-import ResultTable from './components/ResultTable/ResultTable';
-import UserInput from './components/UserInput/UserInput';
+import { useState } from "react";
+import logo from "./assets/investment-calculator-logo.png";
+import Header from "./components/Header/Header";
+import ResultTable from "./components/ResultTable/ResultTable";
+import UserInput from "./components/UserInput/UserInput";
 
 function App() {
-
+  const [userInput, setUserInput] = useState(null);
 
   const calculateHandler = (userInput) => {
     // Should be triggered when form is submitted
     // You might not directly want to bind it to the submit event on the form though...
 
     console.log(userInput);
+    setUserInput(userInput);
+  };
+
+  console.log(userInput);
+
+  if (userInput) {
     const yearlyData = []; // per-year results
 
-    let currentSavings = +userInput['currentSavings']; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput['yearlyContribution']; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput['expectedReturn'] / 100;
-    const duration = +userInput['duration'];
+    let currentSavings = +userInput["currentSavings"]; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput["yearlyContribution"]; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput["expectedReturn"] / 100;
+    const duration = +userInput["duration"];
 
     // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
@@ -33,14 +40,12 @@ function App() {
 
     // do something with yearlyData ...
     console.log(yearlyData);
-    
-    
-  };
+  }
 
   return (
     <div>
       <Header />
-      <UserInput onCalculate={ calculateHandler} />
+      <UserInput onCalculate={calculateHandler} />
       <ResultTable />
     </div>
   );
